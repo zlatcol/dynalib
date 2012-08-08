d3588f613b3a42bd9673249f847c3e7f67ea3729
Jakob Skwarski <jakob.skwarski@dynabyte.se>
2012-08-08 15:26:03 +0200

    Lägg till lite böcker i dbn

---
+++ conf/evolutions/default/4.sql
@@ -0,0 +1,7 @@
+# ---!Ups
+
+insert into books (title) values ('Flickan som lekte med en sten'),('Stenen som lekte med en bil'),('Bilen som var scrum master inom ett agilt projekt'),('Agila projektet som mest av allt ville vara en sten');
+
+# ---!Downs
+
+delete from books;
