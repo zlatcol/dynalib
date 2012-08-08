14ee615ea611e27ef982ce81cc16a4f7030c3953
Jakob Skwarski <jakob.skwarski@dynabyte.se>
2012-08-08 14:57:26 +0200

    ...

--- app/controllers/DBHandler.scala
+++ app/controllers/DBHandler.scala
@@ -8,24 +8,7 @@ import anorm.SqlParser._
 import models.Book
 
 object DBHandler {
-	/** Old junk **/ 
-	/*val intParser = get[String]("result")~get[String]("operation") map {case id~operation => (id, operation)}
 	
-	def saveResultInDb(result: Any, operation: String) {
-		DB.withConnection { implicit c =>
-			SQL("insert into results (result, operation) values ({result}, {operation})").on(
-				'result -> result,
-				'operation -> operation
-			).executeUpdate()
-		}
-	}
-	
-	def getAllResults(): List[(String, String)] = {
-		DB.withConnection { implicit c =>
-			SQL("select result, operation from results").as(intParser *)
-		}
-	}*/
-
 	val bookParser = get[String]("title") map {case title => new Book(title)}
 	
 	def getTitles(): List[Book] = {
