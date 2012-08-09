e7742e1fab705615c8d21e9a66eb837e86d4f899
Jakob Skwarski <jakob.skwarski@dynabyte.se>
2012-08-09 13:05:48 +0200

    La till ett till test till DynaLibTest

--- test/controllers/DynaLibTest.scala
+++ test/controllers/DynaLibTest.scala
@@ -16,4 +16,14 @@ class DynaLibTest extends Specification {
 			contentAsString(result) must contain("Welcome to DynaLib!")
 		}
 	
+	"respond to the addBook Action" in {
+		val result = controllers.DynaLib.addBook()(FakeRequest())
+			status(result) must equalTo(OK)
+			contentType(result) must beSome("text/html")
+			charset(result) must beSome("utf-8")
+			contentAsString(result) must contain("Add book!")
+			contentAsString(result) must contain("title")
+			contentAsString(result) must contain("pages")
+		}
+	
 }
