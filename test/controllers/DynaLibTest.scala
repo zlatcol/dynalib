4eae527acb87587a82268401008161fcba201a33
Jakob Skwarski <jakob.skwarski@dynabyte.se>
2012-08-09 14:48:11 +0200

    lagt till lite testramverk som ändå inte funkar

--- test/controllers/DynaLibTest.scala
+++ test/controllers/DynaLibTest.scala
@@ -1,12 +1,12 @@
-package controllers
+package controllers2
 
 import org.specs2.mutable._
-
+import org.specs2.mock.Mockito
 import play.api.test._
 import play.api.test.Helpers._
+import controllers.DBHandler
 
-
-class DynaLibTest extends Specification {
+class DynaLibTest extends Specification with Mockito{
 	
 	"respond to the index Action" in {
 		val result = controllers.DynaLib.index()(FakeRequest())
@@ -33,6 +33,6 @@ class DynaLibTest extends Specification {
 			charset(result) must beSome("utf-8")
 			contentAsString(result) must contain("Flickan som lekte med en sten")
 			}
-		}
+	}
 	
 }
