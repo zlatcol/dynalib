47909b81ec017891c8dc8de4a8f0358ecc7a6ffb
Jakob Skwarski <jakob.skwarski@dynabyte.se>
2012-08-10 09:37:38 +0200

    Refactor addBook to use Tuple3 instead of list

--- app/controllers/BookController.scala
+++ app/controllers/BookController.scala
@@ -17,8 +17,7 @@ object BookController extends Controller {
 	 * Skapa Book objekt, skicka till DBn.
 	 */
 	def addBook(bookInfo: (String, String, Int)) {
-		val book = List[(String, String, Int)]((bookInfo._1, bookInfo._2 ,bookInfo._3))
-		DBHandler.addBook(book)
+		DBHandler.addBook(bookInfo)
 	}
 	
 	def getBook(id: Int): Option[Book] = {
--- app/controllers/DBHandler.scala
+++ app/controllers/DBHandler.scala
@@ -9,9 +9,9 @@ import models.Book
 
 object DBHandler {
 	
-	def addBook(book: List[(String, String, Int)]) {
+	def addBook(book: (String, String, Int)) {
 		DB.withConnection { implicit c =>
-			SQL("INSERT INTO books (title, language, pages) VALUES ({title}, {language}, {pages})").on('title -> book(0), 'language -> book(1), 'pages -> book(3)).executeUpdate()
+			SQL("INSERT INTO books (title, language, pages) VALUES ({title}, {language}, {pages})").on('title -> book._1, 'language -> book._2, 'pages -> book._3).executeUpdate()
 		}
 	}
 }
