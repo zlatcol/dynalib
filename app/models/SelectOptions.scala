7406488bb5f51f0d6e8b9460e46136659a68ac9d
Jakob Skwarski <jakob.skwarski@dynabyte.se>
2012-08-15 15:29:24 +0200

    Change borrow time period select to a for-loop

--- app/models/SelectOptions.scala
+++ app/models/SelectOptions.scala
@@ -6,5 +6,14 @@ object SelectOptions {
 			"Swedish",
 			"English"
 	)
+	
+	val timePeriods = List(
+		(7, "A week"),
+		(14, "Two weeks"),
+		(21, "Three weeks"),
+		(30, "A month"),
+		(60, "Two months"),
+		(90, "Three months")
+	)
 
 }
