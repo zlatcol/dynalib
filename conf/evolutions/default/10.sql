29c5b8bb593aa17eff4d7b8654d2691eb4d8348b
Jakob Skwarski <jakob.skwarski@dynabyte.se>
2012-08-17 10:36:55 +0200

    Some sql changes

--- conf/evolutions/default/10.sql
+++ conf/evolutions/default/10.sql
@@ -2,7 +2,7 @@
 
 # --- !Ups
 
-CREATE TABLE book_category (bookId INT(5) NOT NULL, categoryId INT(5) NOT NULL , KEY book_category_idx (bookId, categoryId), KEY category_book_idx (categoryId, bookId)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE book_category (bookId INT(5) NOT NULL, categoryId INT(5) NOT NULL , KEY book_category_idx (bookId, categoryId), KEY category_book_idx (categoryId, bookId));
 
 # --- !Downs
 
