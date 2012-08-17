10178c7555435ebb5acba8e6aeac3f75ce52ad40
Jakob Skwarski <jakob.skwarski@dynabyte.se>
2012-08-17 13:05:41 +0200

    postgres syntax errors!

--- conf/evolutions/default/10.sql
+++ conf/evolutions/default/10.sql
@@ -2,8 +2,13 @@
 
 # --- !Ups
 
-CREATE TABLE book_category (bookId integer NOT NULL, categoryId integer NOT NULL , KEY book_category_idx (bookId, categoryId), KEY category_book_idx (categoryId, bookId));
+CREATE TABLE book_category (bookId integer NOT NULL, categoryId integer NOT NULL);
+CREATE INDEX book_category_idx ON book_category (bookId,categoryId);
+CREATE INDEX author_book_idx ON book_category (categoryId, bookId);
+
 
 # --- !Downs
 
 DROP TABLE book_category;
+DROP INDEX IF EXISTS book_category_idx;
+DROP INDEX IF EXISTS category_book_idx;
--- conf/evolutions/default/5.sql
+++ conf/evolutions/default/5.sql
@@ -2,8 +2,12 @@
 
 # --- !Ups
 
-CREATE TABLE book_author (bookId integer NOT NULL, authorId integer NOT NULL , KEY book_author_idx (bookId, authorId), KEY author_book_idx (authorId, bookId));
+CREATE TABLE book_author (bookId integer NOT NULL, authorId integer NOT NULL);
+CREATE INDEX book_author_idx ON book_author (bookId,authorId);
+CREATE INDEX author_book_idx ON book_author (authorId, bookId);
 
 # --- !Downs
 
 DROP TABLE book_author;
+DROP INDEX IF EXISTS book_author_idx;
+DROP INDEX IF EXISTS author_book_idx;
