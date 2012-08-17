de5f8b18f77d7f2286673245329321d6782d04b8
Jakob Skwarski <jakob.skwarski@dynabyte.se>
2012-08-17 13:49:16 +0200

    skam den som ger sig

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,7 +3,15 @@
 # --- !Ups
 
 CREATE TABLE books (id SERIAL NOT NULL, title VARCHAR(255) NOT NULL DEFAULT '', PRIMARY KEY (id));
+CREATE TABLE book_category (bookId integer NOT NULL, categoryId integer NOT NULL);
+CREATE INDEX book_category_idx ON book_category (bookId,categoryId);
+CREATE INDEX author_book_idx ON book_category (categoryId, bookId);
+CREATE TABLE category (id serial NOT NULL, name VARCHAR(255) NOT NULL DEFAULT '', PRIMARY KEY (id));
 
 # --- !Downs
 
 DROP TABLE books;
+DROP TABLE book_category;
+DROP INDEX IF EXISTS book_category_idx;
+DROP INDEX IF EXISTS category_book_idx;
+DROP TABLE category;
