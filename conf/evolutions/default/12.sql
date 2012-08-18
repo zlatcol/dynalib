155213355817783b4f10fae99093d24d353d78df
John Häggkvist <john.haggkvist@gmail.com>
2012-08-18 16:29:18 +0200

    Test user borrowed a book

--- conf/evolutions/default/12.sql
+++ conf/evolutions/default/12.sql
@@ -5,7 +5,7 @@
 DELETE FROM users;
 ALTER TABLE users ADD COLUMN email varchar(64) default NULL AFTER id;
 CREATE UNIQUE INDEX email_idx ON users(email);
-INSERT INTO users (email, name) VALUES ('test.user@dynabyte.se','Test User');
+INSERT INTO users (id, email, name) VALUES (1, 'test.user@dynabyte.se','Test User');
 
 
 # --- !Downs
--- conf/evolutions/default/13.sql
+++ conf/evolutions/default/13.sql
@@ -3,6 +3,7 @@
 # --- !Ups
 
 ALTER TABLE books CHANGE COLUMN borrowed_by borrowed_by int(5) default NULL;
+UPDATE books SET borrowed_by = 1, date_back = '2012-09-01' WHERE id = 2;
 
 # --- !Downs
 
