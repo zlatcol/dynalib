a7ae28f3b8f2e413cd0103f8f4e90c4e7c7477a9
Jakob Skwarski <jakob.skwarski@dynabyte.se>
2012-08-20 11:26:52 +0200

    Add some categories via evolutions

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,9 @@
+# DynaLib Database
+
+# --- !Ups
+
+INSERT INTO category (name) VALUES ('Frontend'),('Backend'),('Scrum'),('Agile'),('Javascript'),('jQuery'),('HTML5'),('Web 3.0 mod 2');
+
+# --- !Downs
+
+DELETE FROM category;
