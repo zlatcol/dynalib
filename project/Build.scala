760804b123e134b8e033ccb102ff112d33c8ef6f
Jakob Skwarski <jakob.skwarski@dynabyte.se>
2012-09-18 14:32:55 +0200

    Added play2war plugin for making war-files

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,7 @@
 import sbt._
 import Keys._
 import PlayProject._
+import com.github.play2war.plugin._
 
 object ApplicationBuild extends Build {
 
@@ -11,9 +12,17 @@ object ApplicationBuild extends Build {
       "postgresql" % "postgresql" % "8.4-702.jdbc4",
       "org.mockito" % "mockito-all" % "1.9.5-rc1"
     )
+    val projectSettings = Play2WarPlugin.play2WarSettings ++ Seq(
+    	Play2WarKeys.servletVersion := "2.5"
+ 	)
+    
+    val main = PlayProject(
+    	appName, appVersion, appDependencies, mainLang = SCALA
+    ).settings(projectSettings: _*)
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here
-    )
 
 }
+
+//val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+		// Add your own project settings here
+	//	)
